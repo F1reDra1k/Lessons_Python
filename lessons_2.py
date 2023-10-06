6407cb3e5c95492fbc68cdcf7fc4a5a020a3dc9f
F1reDra1k <dhorosenko@gmail.com>
2023-10-06 20:24:06 +0300

    adding lesson 1

--- lessons_2.py
+++ lessons_2.py
@@ -6,4 +6,24 @@
 # float - 2.9
 # complex - (2+3)
 # decimal - 2.999999999
-#
+
+# string - срічка
+# str
+
+# Bolean_bool(True/False)
+
+# list - список []
+# tuple - кортежи ()
+# set - множина {}
+# dict - словник {key: value}
+
+# None
+
+
+box = 1
+
+new_box = box
+
+box = 2
+
+print(box)
