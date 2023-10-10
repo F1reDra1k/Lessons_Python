07330d24f5f59a06b686511d4414fce9ed64a190
F1reDra1k <dhorosenko@gmail.com>
2023-10-10 21:13:06 +0300

    adding lesson 2

---
+++ lessons_3.py
@@ -0,0 +1,75 @@
+###### None types
+
+# value_none = None
+#
+# value_int = 123
+#
+# print(value_int)
+#
+# value_id = id(value_int)
+#
+# value_print = print(value_int)
+#
+# print(value_print, type(value_print))
+
+
+
+###### Bool
+
+# value_bool_1 = True
+# value_bool_2 = False
+#
+# result_num = 11 > 2 # >,<, ==, !=, <=, >=
+#
+# result_str = "hello" == "hello" # ==, !=, <, >, <=, >=, in
+#
+# print(result)
+
+
+##### Type
+
+# value_int = 11
+# value_str = "12"
+#
+# # f"{} {}"
+#
+# result = value_int + int(value_str)
+# print(result)
+
+# value_int = 1
+# value_float = 0.1
+# value_str = "hello"
+# #print(bool(value_str)*2)
+#
+# print(bool(value_str)*2, type(bool(value_str)*2))
+
+
+##### If statement
+ #if
+ #elif
+ #else
+#and, or, not
+
+# if умова:
+#       зроби це
+
+value_int = 1
+
+#if value_int > 0:
+    #print(it)
+
+
+# golf, ternary operator, not
+
+####### input() ### ALWAYS STRING
+
+value_1 = input("Please tupe a number: ")
+value_2 = input("Please tupe another number: ")
+
+result = value_1 + value_2
+
+print(result)
+
+###### Homework
+
+
